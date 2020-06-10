3062f7fc26f8ba6c41abe31aafb6c74e5f48a377
Diego Hernández Ureño <44782479+DiegoUreno@users.noreply.github.com>
2020-06-10 17:00:52 -0500

    paola caca

--- ajax/login.php
+++ ajax/login.php
@@ -1,6 +1,23 @@
 <?php
     include "../conn.php";
 
+
+$user= $_POST["usr"];
+$pass=$_POST["pass"];
+//if(preg_match("^[0-9A-Za-z]+$",$user)&&preg_match("^[0-9A-Za-z]+$",$pass)){
+//    $query ="SELECT * FROM `cliente` WHERE id_boleta='".pg_escape_string($user)."' and pass='".sha1(pg_escape_string($pass))"'";
+//   
+//    $result= $con->query($query);
+//    if($result){
+//      if($result->num_rows==0){
+//          echo "El usuario no existe, por favor verificar o regristarse";
+//      }else
+//          $_SESSION["user"] = ($result->fetch_row())[0];
+//          echo "Pase usted, buena persona";
+//    }
+//    
+//}
+
     $login = $con->query("SELECT * FROM `cliente` WHERE id_boleta = '".$_POST["usr"]."' AND pass = '".sha1($_POST["pass"])."'");
 
     if($login->num_rows > 0){
@@ -8,15 +25,4 @@
     }else{
         echo "El usuario no existe, por favor verificar o registrarse";
     }
- <script src="https://www.google.com/recaptcha/api.js"></script>
-      <script>
-   function onSubmit(token) {
-     document.getElementById("demo-form").submit();
-   }
- </script>
-     <button class="g-recaptcha" 
-        data-sitekey="reCAPTCHA_site_key" 
-        data-callback='onSubmit' 
-        data-action='submit'>Submit</button>
- 
 ?>
